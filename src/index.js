cde4b993ac7b5773023cfd9a61e8e299250abf58
Gideon Macharia <kamaugedion95@gmail.com>
2024-04-06 16:47:42 +0300

    complete challenge

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// Your code here
+document.addEventListener("DOMContentLoaded", ()=>{
+    function movieTitle(movie) {
+        const li = document.querySelector(".form item")
+        li.textContent = movie.title
+        
+    }
+
+})
