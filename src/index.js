31307a5e85a28b2c25efbb7f2c37dcebb34f9ce9
Gideon Macharia <kamaugedion95@gmail.com>
2024-04-07 00:13:23 +0300

    complete challenge

--- src/index.js
+++ src/index.js
@@ -1,9 +1,51 @@
 // Your code here
-document.addEventListener("DOMContentLoaded", ()=>{
+
+    //DOM render fuunctions
     function movieTitle(movie) {
-        const li = document.querySelector(".form item")
-        li.textContent = movie.title
-        
+        // Select the existing <li> element with class "form item"
+        const li = document.getElementsByClassName("film item")
+
+         // Set inner HTML with movie details
+        li.innerHTML += `
+        <p>${movie.id }</p>
+        <p>${movie.title }</p>
+        <p>${movie.runtime}</P>
+        <P>${movie.capacity}</P>
+        <P>${movie.showtime}</P>
+        <P>${movie.tickets_sold}</P>
+        <P>${movie.description}</P>
+        <P>${movie.poster}</P>
+        `;
+        console.log(li)
+
     }
 
-})
+
+    //fetch requests
+    //Get fetch for all users
+    function getAllMovies(){
+        let baseURL = "http://localhost:3000/films"
+        fetch(baseURL)
+        .then(res => res.json())
+        .then(films =>films.forEach(movie => {
+            return movieTitle(movie)}))
+    }
+    
+    // function processFilms(response) {
+    //     return response.json()
+    //         .then(films => {
+    //             films.forEach(movie => {
+    //                 movieTitle(movie);
+    //             });
+    //         });
+    // }
+     //processFilms()
+    
+
+     //initial render
+    //Get data and render movies to the DOM
+    function initialize(){
+        getAllMovies()
+        // 
+    }
+    console.log(initialize())
