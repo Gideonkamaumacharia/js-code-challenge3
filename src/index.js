dcbe44b51bac67d8620540eab719d95ed9805c96
Gideon Macharia <kamaugedion95@gmail.com>
2024-04-08 16:28:43 +0300

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,86 +1,147 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    // Your code here
+// Your code here
+let url = "http://localhost:3000/films/";
+let ulFilms = document.getElementById("films");
+let idBuyticket = document.getElementById("buy-ticket")
+
+let movieImg = document.getElementById("poster");
+let idTitle = document.getElementById("title")
+let idRuntime = document.getElementById("runtime")
+let idFilmInfo = document.getElementById("film-info")
+let idShowtime = document.getElementById("showtime")
+let idTicketnum = document.getElementById("ticket-num")
 
 
-    function handleBuyMovie(e){
-        e.preventDefault()
-        let movieObj = {
-           id: e.target.id.value,
-           title:e.target.title.value,
-           runtime:e.target.runtime.value,
-           capacity:e.target.capacity.value,
-           showtime:e.target.showtime.value,
-           tickets:e.target.tickets_sold.value,
-           description:e.target.description.value,
-           poster:e.target.poster.value
+function grabMovie(){
+    fetch(url)
+    .then(res => res.json())
+    .then(data => { 
+        ulFilms.innerHTML = "";
+        for(values of data){
+             addMovie(values);
         }
-        renderMovie(movieObj)
-        buyMovie(movieObj)
+        }
+    )
+    .catch(e => console.log(e.message));
+}
+grabMovie();
+function addMovie(movies){
+    
+    let remaining = movies.capacity - movies.tickets_sold;
+
+    movieTitle = movies.title
+    movieId = movies.id
+    let liFilm = document.createElement("li");
+    if(!remaining > 0)
+    {  liFilm.className = "sold-out"
     }
 
-    //DOM render fuunctions
-    function renderMovie(movie) {
-        // Select the existing <li> element with class "form item"
-        const li = document.querySelector(".film.item")
-        
-        // if(!li){
-        //     console.error("Could not find element with class 'film item'");
-        // return;
-        // }
-         // Set inner HTML with movie details
-        li.innerHTML = `
-        <p>${movie.id }</p>
-        <p>${movie.title }</p>
-        <p>${movie.runtime}</P>
-        <P>${movie.capacity}</P>
-        <P>${movie.showtime}</P>
-        <P>${movie.tickets_sold}</P>
-        <P>${movie.description}</P>
-        <P>${movie.poster}</P>
-        `;
-        console.log(li)
+    ulFilms.appendChild(liFilm);
 
+    let movieSpan = document.createElement("span");
+    movieSpan.innerText = movieTitle;
+    liFilm.appendChild(movieSpan);
+
+    let deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete"
+    liFilm.appendChild(deleteButton); 
+
+    deleteButton.addEventListener('click', () => {
+        deleteMovie(movies)
+    })
+    movieSpan.addEventListener('click', () => {
+        updateDom(movies);
+    })
+    if(movies.id === "1"){
+        updateDom(movies);
+    }
+}
+
+function updateDom(movies){
+    let remaining = movies.capacity - movies.tickets_sold;
+    let movieId = movies.id;
+    let availabiity;
+
+    if(remaining > 0){
+        availabiity = "Buy Tickt"
+    }else{
+        availabiity = "Sold out"
     }
 
+    movieImg.src = movies.poster; 
+    movieImg.alt = movies.title; 
+    idTitle.innerText = movies.title;
+    idRuntime.innerText = movies.runtime + " minutes";
+    idFilmInfo.innerText = movies.description;
+    idShowtime.innerText = movies.showtime;
+    idTicketnum.innerText = remaining;
 
-    //fetch requests
-    //Get fetch for all users
-    function getAllMovies(){
-        let baseURL = 
-        fetch("http://localhost:3000/films")
-        .then(res => res.json())
-        .then(films =>films.forEach(movie => {
-            return renderMovie(movie)}))
+    idBuyticket.onclick = () => {
+        if(remaining > 0)
+        { 
+             buyTicket(movies)
+        }else{
+            console.log("You cannot buy tickets")
+        }
+    };
+    idBuyticket.dataset.movieId = movies.id;
+    let button = document.querySelector("[data-movie-id='"+movieId+"']");
+    button.innerText = availabiity;
+}
+function buyTicket(movies){
+
+    movies.tickets_sold++
+    let ticketsSold = movies.tickets_sold;
+    let requestHeaders = {
+        "Content-Type": "application/json"
     }
+    let requestBody = {
+        "tickets_sold": ticketsSold
+    }
+    fetch(url+movies.id,{
+        method: "PATCH",
+        headers: requestHeaders,    
+        body: JSON.stringify(requestBody)
+    })
+    .then (res => res.json())
+    .then (data => {
+        updateDom(data);
 
-    function buyMovie(){
-        fetch("http://localhost:3000/films",{
+        let numberOfTickets = (data.capacity - data.tickets_sold)
+
+        if(!numberOfTickets > 0)
+        { grabMovie()
+        }
+
+        let  RequestBodyTickets =  {
+            "film_id": data.id,
+            "number_of_tickets": numberOfTickets
+         }
+
+        fetch("http://localhost:3000/tickets",{
             method: "POST",
-            headers:{
-                'Content-Type': "application/json",
-                'Accept': "application/json"
-            },
-            body:JSON.stringify(movieObj)
-           
+            headers: requestHeaders,    
+            body: JSON.stringify(RequestBodyTickets)
         })
-    }
-    
-    // function processFilms(response) {
-    //     return response.json()
-    //         .then(films => {
-    //             films.forEach(movie => {
-    //                 movieTitle(movie);
-    //             });
-    //         });
-    // }
-     //processFilms()
-    
+        .then (res => res.json())
+        .then(data => data)
+        .catch (e => console.log(e.message));
 
-     //initial render
-    //Get data and render movies to the DOM
-    function initialize(){
-        getAllMovies()
-        
+    })
+    .catch (e => console.log(e.message));
+}
+function deleteMovie(movie){
+    let requestHeaders = {
+        "Content-Type": "application/json"
     }
-    initialize()
-})
+    let requestBody = {
+        "id": movie.id
+    }
+    fetch(url+movie.id, {
+        method: "DELETE",
+        headers: requestHeaders,    
+        body: JSON.stringify(requestBody)
+    })
+    .then (res => res.json())
+    .then (data => grabMovie())
+    .catch (e => console.log(e.message));
+}
