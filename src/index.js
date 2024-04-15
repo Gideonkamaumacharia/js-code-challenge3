7634996aea55988697e3f1a0eb65d6ea263bda23
Gideon Macharia <kamaugedion95@gmail.com>
2024-04-15 13:12:08 +0300

    create grabMovie()

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
-let baseUrl = http://localhost:3000
+let baseUrl = 'http://localhost:3000/films'
 let ul = document.getElementById("films")
 
 
+let li = document.createElement('li')
+
+
+function grabMovie(){
+    fetch(baseUrl)
+    .then(res => res.json())
+    .then(data => {
+        for(values of data){
+            addMovie(values)
+        }
+    })
+
+}
